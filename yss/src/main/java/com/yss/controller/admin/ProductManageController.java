c27615653366b0ddd939b2821fd96c2bf502f8f1
user <user@DESKTOP-D0N4INE>
2026-08-12 17:43:32 +0900

    2026-08-12 17:42 ProductManageController 추가 - 이도윤

---
+++ yss/src/main/java/com/yss/controller/admin/ProductManageController.java
@@ -0,0 +1,10 @@
+package com.yss.controller.admin;
+
+import com.yss.mvc.annotation.Controller;
+import com.yss.mvc.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/product/*")
+public class ProductManageController {
+
+}
